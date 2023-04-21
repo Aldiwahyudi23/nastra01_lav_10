09c7997a3bad2709928476f092f0806c1a624c43
Aldiwahyudi23 <aldiwahyudi1223@gmail.com>
2023-04-21 12:51:37 +0700

    errot

--- resources/views/peraturan/index.blade.php
+++ resources/views/peraturan/index.blade.php
@@ -78,7 +78,7 @@
                                     </tr>
                                     <tr>
                                         <td width="50"><i class="nav-icon fas fa-list"></i></td>
-                                        <td> <a href="{{Route('role.show',Crypt::encrypt(5)))}} " class="text-dark">Cek Tugas<a></td>
+                                        <td> <a href="{{Route('role.show',Crypt::encrypt(5))}} " class="text-dark">Cek Tugas<a></td>
                                     </tr>
                                 </table>
                             </div>
